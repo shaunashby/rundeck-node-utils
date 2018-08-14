d64b38af56d1924da1356266850944a4772d7356
Shaun Ashby <shaun.ashby@gmail.com>
2018-08-14 17:48:07 +0200

    Update formatters/xml for new data format and return file content

--- src/rundeck/formatters/xml.go
+++ src/rundeck/formatters/xml.go
@@ -2,25 +2,35 @@ package formatters
 
 import "fmt"
 import "foreman"
+import "bytes"
 
 type NodeXmlFormatter struct {
-	HostData *foreman.ForemanHost
+	HostData []foreman.ForemanHost
 }
 
 // Output node to XML format using a template (eventually):
 func (f *NodeXmlFormatter) String() string {
 	defType := "Node"
-	return fmt.Sprintf("<node type=\"%s\" name=\"%s\" />", defType, f.HostData.Name())
-}
+	defUser := "rundeck"
 
-// Node contents:
-// <node>
-// name -> hostname
-// hostname -> IP address
-// description -> comment
-// osArch -> Arch
-// tags: x,y,z
-// username: rundeck
-// attributes: puppetmaster,
-// attributes: hostgroup
-// </node>
+	var nodeStringBuffer bytes.Buffer
+
+	// Top-level XML tag is project:
+	fmt.Fprintf(&nodeStringBuffer,"<project>\n")
+
+	// Iterate over the hosts in the host array:
+	for _, host := range f.HostData {
+		fmt.Fprintf(&nodeStringBuffer,"     <node name=\"%s\" type=\"%s\"\n", host.Name(), defType)
+		fmt.Fprintf(&nodeStringBuffer,"         hostname=\"%s\"\n", host.IpAddress())
+		fmt.Fprintf(&nodeStringBuffer,"         description=\"%s\"\n", host.Comment())
+		fmt.Fprintf(&nodeStringBuffer,"         osArch=\"%s\"\n", host.Arch())
+		fmt.Fprintf(&nodeStringBuffer,"         puppetmaster=\"%s\"\n", host.Puppetmaster())
+		fmt.Fprintf(&nodeStringBuffer,"         hostgroup=\"%s\"\n", host.Hostgroup())
+		fmt.Fprintf(&nodeStringBuffer,"         username=\"%s\" />\n", defUser)
+		// TODO: add tags derived from Foreman metadata:
+		// tags: x,y,z
+	}
+	fmt.Fprintf(&nodeStringBuffer,"</project>\n")
+
+	return nodeStringBuffer.String()
+}

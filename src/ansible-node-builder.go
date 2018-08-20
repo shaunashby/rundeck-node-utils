b8613f8e8dfe009e00e52421f320817958dff673
Shaun Ashby <shaun.ashby@gmail.com>
2018-08-20 11:34:30 +0200

    Add formatters for Ansible hosts file generation

---
+++ src/ansible-node-builder.go
@@ -0,0 +1,57 @@
+//____________________________________________________________________
+// File: ansible-node-builder.go
+//____________________________________________________________________
+//
+// Author: Shaun Ashby <shaun@ashby.ch>
+// Created: 2018-08-20 10:58:18+0200
+// Revision: $Id$
+// Description: Read JSON file and create a node list compatible with Ansible
+//
+// Copyright (C) 2018 Shaun Ashby
+//
+//
+//--------------------------------------------------------------------
+package main
+
+import "fmt"
+import "encoding/json"
+import "os"
+import "io/ioutil"
+import "foreman"
+import "ansible/formatters"
+
+// ReadHostFile reads the JSON input stream and returns
+// a ForemanHost instance containing the unmarshalled
+// JSON data:
+func ReadHostFile(filename string) *foreman.ForemanApiResponse {
+
+	// Open the host file and read in the JSON stream:
+	hfileBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(fmt.Sprintf("Error reading from file %s: %s\n", filename, err))
+	}
+
+	fresp := foreman.ForemanApiResponse{}
+
+	if err := json.Unmarshal(hfileBytes, &fresp); err != nil {
+		panic(fmt.Sprintf("Error unmarshalling from file: %s\n", err))
+	}
+
+	return &fresp
+}
+
+func main() {
+	// Set the zone (office/infra/clients):
+	puppetZone := "infra"
+	var hostDataFilename = fmt.Sprintf("%s/%s.json", os.Getenv("HOME"), puppetZone)
+
+	fresponse := ReadHostFile(hostDataFilename)
+
+	// Choose a formatter:
+	out := &formatters.NodeIniFormatter{
+		HostData: fresponse.GetHosts(),
+		PuppetZone: puppetZone,
+	}
+	// Print the formatted node data for Rundeck:
+	fmt.Printf("%v\n",out)
+}
---
+++ src/ansible/formatters/ini.go
@@ -0,0 +1,38 @@
+//____________________________________________________________________
+// File: ini.go
+//____________________________________________________________________
+//
+// Author: Shaun Ashby <shaun@ashby.ch>
+// Created: 2018-08-20 11:21:53+0200
+// Revision: $Id$
+// Description: Formatter compatible with Ansible hosts file
+//
+// Copyright (C) 2018 Shaun Ashby
+//
+//
+//--------------------------------------------------------------------
+package formatters
+
+import "fmt"
+import "foreman"
+import "bytes"
+
+type NodeIniFormatter struct {
+	HostData []foreman.ForemanHost
+	PuppetZone string
+}
+
+// Write out the data in host file format:
+func (f *NodeIniFormatter) String() string {
+
+	var nodeStringBuffer bytes.Buffer
+
+	// Top-level XML tag is project:
+	fmt.Fprintf(&nodeStringBuffer,"[%s]\n",f.PuppetZone)
+
+	// Iterate over the hosts in the host array:
+	for _, host := range f.HostData {
+		fmt.Fprintf(&nodeStringBuffer,"%-30s ansible_ssh_host=\"%s\"\n", host.Name(), host.IpAddress())
+	}
+	return nodeStringBuffer.String()
+}
